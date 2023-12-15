03485eadcb04a9e032b0a5a94d938f0e60b102ce
Anatoly Chernov <chertoly@gmail.com>
2023-12-16 00:13:29 +0200

    Refine fzf options

--- zsh/zdotdir/cd.sh
+++ zsh/zdotdir/cd.sh
@@ -1,6 +1,6 @@
 function move-to-near-dir {
   local dir
-  dir=$(fd --type d --strip-cwd-prefix --hidden --follow -d 2 | fzf)
+  dir=$(fd --type d --strip-cwd-prefix --hidden --follow -d 2 | fzf --reverse --height 100% --preview 'tree -C {} | head -200')
 
   if [ -n "$dir" ]; then
     cd "$dir"
