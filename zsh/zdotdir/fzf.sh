7d2f0f76d5f0d9475a4a8c4a63953e039de0f82f
Anatoly Chernov <chertoly@gmail.com>
2023-12-16 02:27:56 +0200

    unix-portable-way-to-get-scripts-absolute-path-in-zsh?

--- zsh/zdotdir/fzf.sh
+++ zsh/zdotdir/fzf.sh
@@ -1,4 +1,5 @@
 export FZF_DEFAULT_COMMAND='fdfind --type f --strip-cwd-prefix --hidden --follow --exclude .git'
 
+# https://unix.stackexchange.com/questions/76505/unix-portable-way-to-get-scripts-absolute-path-in-zsh
 . $0:a:h/fzf/change-directory.sh
 . $0:a:h/fzf/edit-file.sh
