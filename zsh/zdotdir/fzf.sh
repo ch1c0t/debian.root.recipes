15936983225f4d4ad518511c8417b7a39ea66a14
Anatoly Chernov <chertoly@gmail.com>
2023-12-17 21:54:53 +0200

    export FZF_DEFAULT_OPTS

--- zsh/zdotdir/fzf.sh
+++ zsh/zdotdir/fzf.sh
@@ -1,4 +1,5 @@
 export FZF_DEFAULT_COMMAND='fdfind --type f --strip-cwd-prefix --hidden --follow --exclude .git'
+export FZF_DEFAULT_OPTS='--reverse --height 100%'
 
 . $(dirname $0)/fzf/change-directory.sh
 . $(dirname $0)/fzf/edit-file.sh
