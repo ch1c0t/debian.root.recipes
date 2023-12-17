ea6618456884e476268efd58d9bc8ab0233d15f0
Anatoly Chernov <chertoly@gmail.com>
2023-12-17 03:21:17 +0200

    Revert "/etc/mu/recipes in zsh"
    This reverts commit 54e5e97942a3f69d9e2eb72dbfc35077f9e3643d.

--- zsh/zdotdir/fzf.sh
+++ zsh/zdotdir/fzf.sh
@@ -1,4 +1,4 @@
 export FZF_DEFAULT_COMMAND='fdfind --type f --strip-cwd-prefix --hidden --follow --exclude .git'
 
-. $0:a:h/fzf/change-directory.sh
-. $0:a:h/fzf/edit-file.sh
+. $(dirname $0)/fzf/change-directory.sh
+. $(dirname $0)/fzf/edit-file.sh
