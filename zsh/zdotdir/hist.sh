57223303ab4c9187d25ae8c59571468ece0d7df1
Anatoly Chernov <chertoly@gmail.com>
2023-12-21 23:36:00 +0200

    - setopt HIST_EXPIRE_DUPS_FIRST

--- zsh/zdotdir/hist.sh
+++ zsh/zdotdir/hist.sh
@@ -2,8 +2,6 @@ HISTFILE=~/.local/share/zsh/history
 HISTSIZE=100000
 SAVEHIST=100000
 
-setopt HIST_EXPIRE_DUPS_FIRST
-
 autoload -Uz up-line-or-beginning-search down-line-or-beginning-search
 zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
