4990935ff1e4fedc7991f44861aae257f254c3bc
Anatoly Chernov <chertoly@gmail.com>
2023-12-21 22:09:19 +0200

    When USER == 'root'

--- user.rb
+++ user.rb
@@ -1,19 +1,23 @@
-USER = u = ENV['MUSER']
-HOME = "/home/#{u}"
+USER = ENV['MUSER']
+HOME = USER == 'root' ? '/root' : "/home/#{USER}"
 
-user :create do
-  username u
-  home HOME
-  create_home true
-  shell '/bin/zsh'
+unless USER == 'root'
+  user :create do
+    username USER
+    home HOME
+    create_home true
+    shell '/bin/zsh'
+  end
 end
 
 include_recipe 'base'
 include_recipe 'zsh'
 include_recipe 'tmux'
-include_recipe 'i3'
+include_recipe 'i3' unless USER == 'root'
 include_recipe 'git'
 include_recipe 'vim'
 
-execute "chmod -R 700 #{HOME}"
-execute "chown -R #{USER}:#{USER} #{HOME}"
+unless USER == 'root'
+  execute "chmod -R 700 #{HOME}"
+  execute "chown -R #{USER}:#{USER} #{HOME}"
+end
