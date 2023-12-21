c07ab766dcf887a0d8823159df9cd98f0129295a
Anatoly Chernov <chertoly@gmail.com>
2023-12-21 23:13:49 +0200

    Trying to fix tmux quirks

--- zsh/zdotdir/bindkey.sh
+++ zsh/zdotdir/bindkey.sh
@@ -1,3 +1,6 @@
+# https://superuser.com/questions/403355/how-do-i-get-searching-through-my-command-history-working-with-tmux-and-zshell
+bindkey -e
+
 bindkey '^j' autosuggest-execute
 bindkey -s '\eh' '^l bindkey -l^M'
 bindkey -s '\eeh' '^l bindkey -l^M'
