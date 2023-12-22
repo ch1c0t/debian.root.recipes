ff5eaca2155c7b5f03cfbc38df8a2765a6bed3c9
Anatoly Chernov <chertoly@gmail.com>
2023-12-22 06:07:55 +0200

    Use https://cloud.r-project.org/bin/linux/debian

--- apt.rb
+++ apt.rb
@@ -1,5 +1,5 @@
 directory '/etc/apt/sources.list.d'
 
-apt_repository 'deb http://cloud.r-project.org/bin/linux/debian bookworm-cran40/' do
+apt_repository 'deb https://cloud.r-project.org/bin/linux/debian bookworm-cran40/' do
   gpg_key '95C0FAF38DB3CCAD0C080A7BDC78B2DDEABC47B7'
 end
