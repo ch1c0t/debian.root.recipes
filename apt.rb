259569e3ee1c714bb4a7ed376708c5049f441937
Anatoly Chernov <chertoly@gmail.com>
2023-12-23 04:53:36 +0200

    Provide /etc/apt/sources.list

--- apt.rb
+++ apt.rb
@@ -1,5 +1,1 @@
-directory '/etc/apt/sources.list.d'
-
-apt_repository 'deb https://cloud.r-project.org/bin/linux/debian bookworm-cran40/' do
-  gpg_key '95C0FAF38DB3CCAD0C080A7BDC78B2DDEABC47B7'
-end
+include_recipe 'apt'
---
+++ apt/default.rb
@@ -0,0 +1,9 @@
+remote_file '/etc/apt/sources.list' do
+  source 'sources.list'
+end
+
+directory '/etc/apt/sources.list.d'
+
+apt_repository 'deb https://cloud.r-project.org/bin/linux/debian bookworm-cran40/' do
+  gpg_key '95C0FAF38DB3CCAD0C080A7BDC78B2DDEABC47B7'
+end
