e6e8d4ffc056989c6d828315dae76c7d34cd2905
Anatoly Chernov <chertoly@gmail.com>
2024-01-10 05:31:02 +0200

    htop: make it optional

--- user.rb
+++ user.rb
@@ -5,7 +5,6 @@ include_recipe 'tmux'
 include_recipe 'i3' unless USER == 'root'
 include_recipe 'git'
 include_recipe 'vim'
-include_recipe 'htop'
 
 unless USER == 'root'
   execute "chown -R #{USER}:#{USER} #{HOME}"
