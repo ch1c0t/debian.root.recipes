1e4dce3f2ec0002edc8f2527dec6bc5b676618c8
Anatoly Chernov <chertoly@gmail.com>
2024-02-06 04:13:52 +0200

    zsh: pay attention to the error codes
    https://unix.stackexchange.com/questions/703918/print-exit-status-code-after-each-command-in-terminal/703925#703925

--- zsh/zdotdir/prompt.sh
+++ zsh/zdotdir/prompt.sh
@@ -3,11 +3,11 @@ setopt PROMPT_SUBST
 autoload -Uz vcs_info
 precmd() {
   vcs_info
-  print -rP "%(?.%F{green}●%f.%F{red}●%f) %(!.%F{red}%n%f.%n) %F{211}%3~%f ${vcs_info_msg_0_}"
+  print -rP "%F{green}●%f %(!.%F{red}%n%f.%n) %F{211}%3~%f ${vcs_info_msg_0_}"
 }
 
 zstyle ':vcs_info:*' enable git
 zstyle ':vcs_info:git:*' check-for-changes true
 zstyle ':vcs_info:git:*' formats '%F{153}%b%f%F{red}%c%u%m%f'
 
-PROMPT="%# "
+PROMPT="%(?..%F{red}%?%f)%# "
