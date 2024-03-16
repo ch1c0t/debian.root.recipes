dff094f86230dd043aac4e99911d3f4f1fcce655
Anatoly Chernov <chertoly@gmail.com>
2024-03-16 15:53:08 +0200

    Use chruby only if available

--- zsh/zdotdir/chruby.sh
+++ zsh/zdotdir/chruby.sh
@@ -1,3 +1,5 @@
-source ~/opt/share/chruby/chruby.sh
-source ~/opt/share/chruby/auto.sh
-chruby 3.3.0
+if [ -d ~/opt/share/chruby ]; then
+  source ~/opt/share/chruby/chruby.sh
+  source ~/opt/share/chruby/auto.sh
+  chruby 3.3.0
+fi
