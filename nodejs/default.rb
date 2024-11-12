3402b47b36bc6ebe805bfb43cef0d59344b5abc1
Anatoly Chernov <chertoly@gmail.com>
2024-11-12 16:11:47 +0200

    Use mise instead of nvm for Node.js

--- nodejs/default.rb
+++ nodejs/default.rb
@@ -1,9 +1,1 @@
-git :sync do
-  not_if "test -d #{HOME}/.nvm"
-  repository 'https://github.com/nvm-sh/nvm'
-  destination "#{HOME}/.nvm"
-end
-
-execute 'zsh -c "source ~/.config/zsh/nvm.sh && \
-nvm install lts/iron && \
-nvm alias default lts/iron"'
+execute '~/.local/bin/mise use -g node@22'
--- zsh/zdotdir/.zshrc
+++ zsh/zdotdir/.zshrc
@@ -10,7 +10,7 @@
 . $ZDOTDIR/mu.sh
 . $ZDOTDIR/bindkey.sh
 . $ZDOTDIR/hist.sh
-. $ZDOTDIR/nvm.sh
 . $ZDOTDIR/chruby.sh
+. $ZDOTDIR/mise.sh
 [[ -d "$HOME/Android" ]] && . $ZDOTDIR/android.sh
 [[ -d "$HOME/archive/flutter/flutter" ]] && . $ZDOTDIR/flutter.sh
