be719bc1c828ad8254e57685561fb6f0efa7cd88
Anatoly Chernov <chertoly@gmail.com>
2024-11-17 10:49:52 +0200

    Make ./init work

--- tmux/default.rb
+++ tmux/default.rb
@@ -4,22 +4,22 @@ remote_file "#{HOME}/.config/tmux/tmux.conf" do
   source 'tmux.conf'
 end
 
-remote_file "#{HOME}/.local/bin/to-left-pane" do
+remote_file "/usr/local/bin/to-left-pane" do
   source 'to-left-pane'
-  mode '700'
+  mode '755'
 end
 
-remote_file "#{HOME}/.local/bin/to-lower-pane" do
+remote_file "/usr/local/bin/to-lower-pane" do
   source 'to-lower-pane'
-  mode '700'
+  mode '755'
 end
 
-remote_file "#{HOME}/.local/bin/to-upper-pane" do
+remote_file "/usr/local/bin/to-upper-pane" do
   source 'to-upper-pane'
-  mode '700'
+  mode '755'
 end
 
-remote_file "#{HOME}/.local/bin/to-right-pane" do
+remote_file "/usr/local/bin/to-right-pane" do
   source 'to-right-pane'
-  mode '700'
+  mode '755'
 end
--- vim/default.rb
+++ vim/default.rb
@@ -1,8 +1,3 @@
 remote_directory "#{HOME}/.vim" do
   source 'rc'
 end
-
-remote_file "#{HOME}/.local/bin/vim-helptags" do
-  source 'helptags/script'
-  mode '700'
-end
