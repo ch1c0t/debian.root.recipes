ca69cee13df2337d64b183479bf85c913dc35b3e
Anatoly Chernov <chertoly@gmail.com>
2024-12-28 18:08:15 +0200

    Remove packages in /root/recipes/security/packages_to_remove

--- security/default.rb
+++ security/default.rb
@@ -1,4 +1,4 @@
-execute 'xargs -a /etc/mu/recipes/security/packages_to_remove apt remove -y'
+execute 'xargs -a /root/recipes/security/packages_to_remove apt remove -y'
 
 service 'avahi-daemon' do
   action [:stop, :disable]
