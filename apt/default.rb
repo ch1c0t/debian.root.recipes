62299fb4b44212bbe8339f4474bc87414c8d9c9c
Anatoly Chernov <chertoly@gmail.com>
2025-06-13 23:42:13 +0300

    Install VSCodium

--- apt/default.rb
+++ apt/default.rb
@@ -5,9 +5,8 @@ end
 
 directory '/etc/apt/sources.list.d'
 
-apt_repository 'deb https://cloud.r-project.org/bin/linux/debian bookworm-cran40/' do
-  gpg_key '95C0FAF38DB3CCAD0C080A7BDC78B2DDEABC47B7'
-end
+include_recipe 'r'
+include_recipe 'vscodium'
 
 package 'findutils'
 package 'apt'
---
+++ apt/vscodium.rb
@@ -0,0 +1,16 @@
+commands = """
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
+    | gpg --dearmor \
+    | dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
+apt update
+"""
+
+execute commands do
+  not_if "test -f /usr/share/keyrings/vscodium-archive-keyring.gpg"
+end
+
+file "/etc/apt/sources.list.d/vscodium.list" do
+  content "deb [arch=amd64,arm64 signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg] https://download.vscodium.com/debs vscodium main"
+end
+
+package 'codium'
