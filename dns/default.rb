dcc8383da82ff4efd7f6c7b6f7e5db24e4e061f9
Anatoly Chernov <chertoly@gmail.com>
2026-02-02 12:13:28 +0200

    dns: nmcli ignore-auto-dns

--- dns/default.rb
+++ dns/default.rb
@@ -1,3 +1,6 @@
+execute 'nmcli connection modify "Wired connection 1" ipv4.ignore-auto-dns yes'
+execute 'nmcli connection modify "Wired connection 1" ipv6.ignore-auto-dns yes'
+
 file '/etc/resolv.conf' do
   content <<~S
     nameserver 8.8.8.8
