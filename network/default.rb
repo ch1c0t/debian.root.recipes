0af64681efef4d3bd8e2c2fd46c50899045e19d1
Anatoly Chernov <chertoly@gmail.com>
2026-02-09 12:13:53 +0200

    dns: use dnscrypt-proxy
    https://wiki.archlinux.org/title/Dnscrypt-proxy
    https://dnscrypt.info/faq/
    https://github.com/DNSCrypt/dnscrypt-proxy/wiki/Configuration
    https://wiki.debian.org/dnsmasq
    
    /usr/lib/systemd/system/dnscrypt-proxy.socket
    /etc/dnscrypt-proxy/dnscrypt-proxy.toml

--- network/default.rb
+++ network/default.rb
@@ -1,3 +1,6 @@
+package 'dnsmasq'
+package 'dnscrypt-proxy'
+
 execute """
 nmcli connection modify 'Wired connection 1' ipv4.ignore-auto-dns yes
 nmcli connection modify 'Wired connection 1' ipv4.dns 127.0.0.1
@@ -9,4 +12,5 @@ remote_file '/etc/NetworkManager/NetworkManager.conf'
 remote_file '/etc/NetworkManager/dnsmasq.d/local.conf'
 
 execute 'systemctl disable --now dnsmasq.service'
+execute 'service dnscrypt-proxy restart'
 execute 'service NetworkManager restart'
--- zsh/zdotdir/.zprofile
+++ zsh/zdotdir/.zprofile
@@ -1,7 +1,7 @@
 if [[ ! $DISPLAY && $XDG_VTNR -eq 4 ]]; then
   if [[ ! $TMUX ]]; then
-    tmux new-session -d -s 0
-    tmux send-keys -t 0 'habit-sequence' C-m
+    tmux new-session -d -s 0 'journalctl --since today --follow'
+    tmux new-window -t '$0' 'habit-sequence'
     tmux attach
   fi
 fi
